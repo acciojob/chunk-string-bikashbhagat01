9d2a7757f14e477ca52dbda5a5801ac65c20d9d5
Bikash Bhagat <bikash.bhagat01@gmail.com>
2024-09-15 21:29:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 function stringChop(str, size) {
-  // Return an empty array if the input string is null or size is not positive
-  if (!str || size <= 0) return [];
+	 if(!str || size<=0)
+		 return [];
 
-  // Initialize an array to store the chunks
-  const chunks = [];
+	const chunks = [];
 
-  // Loop through the string, taking slices of the given size
-  for (let i = 0; i < str.length; i += size) {
-    chunks.push(str.slice(i, i + size));
-  }
+	for(let i = 0 ; i < str.length ; i+=size) {
+		chunks.push(str.slice(i, i+size));
+	}
 
-  return chunks;
+	return chunks;
+	
 }
 
 // Do not change the code below
-const str = prompt("Enter String.");
-const size = prompt("Enter Chunk Size.");
-alert(stringChop(str, size));
+// const str = prompt("Enter String.");
+// const size = prompt("Enter Chunk Size.");
+// alert(stringChop(str, size));
