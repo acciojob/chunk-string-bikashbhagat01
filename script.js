a5556f00ee8fae9e2cd4604ad41fbf06ab7ef3cb
Bikash Bhagat <bikash.bhagat01@gmail.com>
2024-09-15 20:01:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function stringChop(str, size) {
-  // your code here
+  // Return an empty array if the input string is null or size is not positive
+  if (!str || size <= 0) return [];
+
+  // Initialize an array to store the chunks
+  const chunks = [];
+
+  // Loop through the string, taking slices of the given size
+  for (let i = 0; i < str.length; i += size) {
+    chunks.push(str.slice(i, i + size));
+  }
+
+  return chunks;
 }
 
 // Do not change the code below
